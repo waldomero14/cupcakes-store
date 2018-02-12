322a06a117b22cb187551937659c4d9818e8ce54
waldomero14 <wivan.vel@gmail.com>
2018-02-11 21:20:18 -0500

    Clear cache issue test

--- sites/all/modules/custom/cupcakes_carousel/js/cupcakes_carousel.js
+++ sites/all/modules/custom/cupcakes_carousel/js/cupcakes_carousel.js
@@ -1,7 +1,6 @@
 (function ($, Drupal) {
     Drupal.behaviors.images_carousel = {
         attach: function (context, settings) {
-            console.log('ratas2');
             $('.carousel-field').slick({autoplay: true, centerMode: true, dots: true, fade: true, speed: 120})
         }
     };
